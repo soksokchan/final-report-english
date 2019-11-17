b79fba076a17fb61ea655c795dee3453cf1a9d6b
soksokchan <soksokchan@users.noreply.github.com>
2019-11-17 18:06:58 +0800

    f
    d

---
+++ script.js
@@ -0,0 +1,16 @@
+function next() {
+    var part_a = document.getElementById("part_a");
+    var part_b = document.getElementById("part_b");
+        part_a.style.display = "none";       
+        part_b.style.display = "block";
+
+
+}
+function next_1() {
+    var part_a = document.getElementById("part_a");
+    var part_b = document.getElementById("part_b");
+        part_b.style.display = "none";       
+        part_a.style.display = "block";
+
+
+}
